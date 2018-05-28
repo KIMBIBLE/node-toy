5e5cd913af484bb2df06fb4ae2bbb23a1b849330
Oss9935 <jotun9935@gmail.com>
2018-05-28 15:56:41 +0900

    populate

---
+++ mongoose_test/populate.js
@@ -0,0 +1,24 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var personSchema = Schema({
+  _id: Schema.Types.ObjectId,
+  name: String,
+  age: Number,
+  stories: [{ type: Schema.Types.ObjectId, ref: 'Story' }]
+});
+// Our Person model has its stories field set to an array of ObjectIds.
+
+// The ref option is what tells Mongoose which model
+// to use during population, in our case the Story model.
+
+// All _ids we store here must be document _ids from the Story model.
+
+var storySchema = Schema({
+  author: { type: Schema.Types.ObjectId, ref: 'Person' },
+  title: String,
+  fans: [{ type: Schema.Types.ObjectId, ref: 'Person' }]
+});
+
+var Story = mongoose.model('Story', storySchema);
+var Person = mongoose.model('Person', personSchema);
