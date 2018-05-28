703068227ecbda8cba25975058e34ecff1fada71
Oss9935 <jotun9935@gmail.com>
2018-05-28 16:17:02 +0900

    populate

--- mongoose_test/populate.js
+++ mongoose_test/populate.js
@@ -1,6 +1,15 @@
 var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test');
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error'));
+db.once('open', function(){
+    console.log('connected');
+});
+
 var Schema = mongoose.Schema;
 
+/* make schema */
 var personSchema = Schema({
   _id: Schema.Types.ObjectId,
   name: String,
@@ -20,5 +29,57 @@ var storySchema = Schema({
   fans: [{ type: Schema.Types.ObjectId, ref: 'Person' }]
 });
 
+/* compile schema to model */
 var Story = mongoose.model('Story', storySchema);
 var Person = mongoose.model('Person', personSchema);
+
+/* make instance of model which corresponding to collection's document */
+var author = new Person({
+  _id: new mongoose.Types.ObjectId(),
+  name: 'Ian Fleming',
+  age: 50
+});
+
+author.save(function (err) {
+  if (err) return handleError(err);
+
+  var story1 = new Story({
+    title: 'Casino Royale',
+    author: author._id    // assign the _id from the person
+  });
+
+  story1.save(function (err) {
+    if (err) return handleError(err);
+    // thats it!
+  });
+});
+
+
+/* find data from mongodb */
+/*
+> db.people.find().pretty()
+{
+	"_id" : ObjectId("5b0baa7feba11a448e9faee2"),
+	"stories" : [ ],
+	"name" : "Ian Fleming",
+	"age" : 50,
+	"__v" : 0
+}
+> db.stories.find().pretty()
+{
+	"_id" : ObjectId("5b0baa7feba11a448e9faee3"),
+	"fans" : [ ],
+	"title" : "Casino Royale",
+	"author" : ObjectId("5b0baa7feba11a448e9faee2"),
+	"__v" : 0
+}
+> db.people.find({name:'Ian Fleming'}).pretty()
+{
+	"_id" : ObjectId("5b0baa7feba11a448e9faee2"),
+	"stories" : [ ],
+	"name" : "Ian Fleming",
+	"age" : 50,
+	"__v" : 0
+}
+> db.people.find({name:'Ian Flemin'}).pretty()
+*/
